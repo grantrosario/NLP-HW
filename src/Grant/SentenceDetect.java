dbf100671f5bbd2cc935e6261e3507a353aa4dd7
Grant Rosario <grantster1124@aol.com>
2017-02-03 23:38:53 -0500

    Initial Commit. Detect Sentences done

---
+++ src/Grant/SentenceDetect.java
@@ -0,0 +1,44 @@
+package Grant;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import opennlp.tools.sentdetect.SentenceModel;
+import opennlp.tools.sentdetect.SentenceDetectorME;
+public class SentenceDetect {
+
+	public static void main(String[] args) throws Exception {
+		
+		String fileName = "news article.txt";
+		StringBuilder strFile = new StringBuilder();
+		BufferedReader textReader = new BufferedReader(new FileReader(fileName));
+		char[] buffer = new char[512];
+		int num = 0;
+		
+		// Loading sentence detector model
+		InputStream inputStream = new FileInputStream("en-sent.bin");
+		SentenceModel model = new SentenceModel(inputStream);
+		
+		// Instantiating the sentenceDetectorME class
+		SentenceDetectorME detector = new SentenceDetectorME(model);
+		
+		// Assigning text file to StringBuilder variable
+		while((num = textReader.read(buffer)) != -1) {
+				String current = String.valueOf(buffer, 0, num);
+				strFile.append(current);
+				buffer = new char[512];
+		}
+		
+		// Detecting the sentences
+		String sentences[] = detector.sentDetect(strFile.toString());
+		
+		// Close BufferedReader
+		textReader.close();
+		
+		// Printing the sentences
+		for(String sent : sentences)
+			System.out.println(sent);
+	}
+
+}
---
+++ src/Grant/SentenceTokenize.java
@@ -0,0 +1,20 @@
+package Grant;
+
+import opennlp.tools.tokenize.WhitespaceTokenizer;
+public class SentenceTokenize {
+
+	public static void main(String[] args) {
+		
+		String sentence = "This is a certain sentence containing some words and what not according to my say so.";
+		
+		WhitespaceTokenizer whitespaceTokenizer = WhitespaceTokenizer.INSTANCE;
+		
+		String tokens[] = whitespaceTokenizer.tokenize(sentence);
+		
+		for(String token : tokens) {
+			System.out.println(token);
+		}
+
+	}
+
+}
